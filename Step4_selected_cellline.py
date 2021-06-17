445a6e642cdab2dd778ea103ff89c78712d541ff
GunHee Lee <44331507+jojo9103@users.noreply.github.com>
2021-06-17 15:09:57 +0900

    Update Step4_selected_cellline.py

--- Step4_selected_cellline.py
+++ Step4_selected_cellline.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 LINCS='/home/jojo9103/LINCS/LINCS/'
+# selected cell line info
 s_LINCS=pd.read_csv(f'{LINCS}/LINCS_Cell.txt',sep='\t')
 
 new_inst_id=pd.read_csv(f'{LINCS}/inst_id_new_inst_id_20210125.txt',sep='\t')
@@ -24,10 +25,13 @@ for dg in set(new_inst_id['pert_iname']):
         mat['Entrez']=pd.to_numeric(mat['Entrez'])
         mat=mat.sort_values(by=['Entrez'],axis=0)
         selected=['pr_gene_id','Entrez','Symbol']
+        # 원하는 Cell line들만 가져오기
         selected1=[i for i in mat.columns if i.split('_')[0] in s_LINCS['cell_id'].values]
         selected_1=[i.split('_') for i in selected1]
+        # DMSO가 아닌경우 농도와 시간이 있는데, 농도 기준 정렬후에 시간기준 정렬함.
         if dg!='DMSO':
             selected1=sorted(selected_1,key=lambda x : (float(x[2]),float(x[1])))
+        # DMSO가 아닌경우 농도와 시간이 있는데, 시간기준 정렬함.
         if dg=='DMSO':
             selected1=sorted(selected_1,key=lambda x : float(x[1]))
         selected1=['_'.join(i) for i in selected1]
